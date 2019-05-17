311f55adb1bc59b538c8eb0160f6c1ff1ca9d12d
Jeremy Lincoln <jeremy.lincoln@biscorp.local>
2019-05-17 09:11:48 -0400

    Added math class, getting a random number for 1-6 to display the correct image, refactor the code to use a funciton called diceroll

--- lib/main.dart
+++ lib/main.dart
@@ -16,26 +16,43 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
-  int leftDiceNumber = 1;
-  int rightDiceNumber = 1;
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1; //set the init state
+  int rightDiceNumber = 1; //set the init state
+  //get a random number
+  var rnd = new Random();
+
+  void diceRoll(){
+    setState(() { //this will let us update the dice
+      leftDiceNumber = 1 + rnd.nextInt(6);
+      rightDiceNumber = 1 + rnd.nextInt(6);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
+          
           Expanded(
             child: FlatButton(
               child: Image.asset('images/dice$leftDiceNumber.png'),
-              onPressed: () {},
+              onPressed: () {
+                diceRoll();                
+              },
             ),
           ),
           Expanded(
             child: FlatButton(
               child: Image.asset('images/dice$rightDiceNumber.png'),
               onPressed: () {
-                print('pressed 2');
+                diceRoll();                 
               },
             ),
           ),
@@ -44,3 +61,4 @@ class DicePage extends StatelessWidget {
     );
   }
 }
+
